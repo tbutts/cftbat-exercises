0d657098529f66e2cfe12531431c99df40e9328e
TB <dapegral@gmail.com>
2017-09-05 17:21:43 -0400

    Clean up chapter 4, include problem text in comments

--- src/cftbat_exercises/4/chpt4.clj
+++ src/cftbat_exercises/4/chpt4.clj
@@ -35,30 +35,37 @@
   [minimum-glitter records]
   (filter #(>= (:glitter-index %) minimum-glitter) records))
 
+; 1. Turn the result of your glitter filter into a list of names.
 (defn glitter-filter-to-name-list
   [minimum-glitter records]
   (map :name (glitter-filter minimum-glitter records)))
 
+; 2. Write a function, append, which will append a new suspect to your list of suspects.
 (defn append
   "Adds a suspect to the list of suspects"
   [vamp-suspects suspect]
   (concat vamp-suspects [suspect]))
 
-(def validators {:name string?
+(def suspect-validators {:name string?
                  :glitter-index number?})
 
+; 3. Write a function, validate, which will check that :name and :glitter-index
+;    are present when you append. The validate function should accept two arguments:
+;    a map of keywords to validating functions, similar to conversions,
+;    and the record to be validated.
 (defn validate
   "Predicate checks that :name and :glitter-index are present"
-  [req-keywords suspect]
-  (reduce (fn [acc [vamp-key value]]
-            (and
-             acc
-             (contains? req-keywords vamp-key)
-             (apply (get req-keywords vamp-key) [value])))
-          true
-          suspect))
+  [validators suspect]
+  (reduce
+   (fn [is-valid [vamp-key value]]
+     (and
+      is-valid
+      (contains? validators vamp-key)
+      ((get validators vamp-key) value)))
+   true
+   suspect))
 
-(def validate-vampire-suspect #(validate validators %))
+(def validate-vampire-suspect #(validate suspect-validators %))
 
 (defn safe-append
   "Validates before appending"
@@ -67,6 +74,8 @@
     (append vamp-suspects suspect)
     vamp-suspects))
 
+; 4. Write a function that will take your list of maps and convert it back to a CSV string.
+;    You'll need to use the clojure.string/join function.
 (defn unparse-line
   [line]
   (clojure.string/join "," (sort-by number? (map second line))))
@@ -76,14 +85,13 @@
   [suspects]
   (clojure.string/join "\n" (map unparse-line suspects)))
 
-; (validate-vampire-suspect {:name "Morvis Fleshdrinking" :glitter-index 5}) ; => true
-; (validate-vampire-suspect {:name "Mr Poopy Butthole" :haha #"10/10"}) ; => false
+(validate-vampire-suspect {:name "Morvis Fleshdrinking" :glitter-index 5}) ; => true
+(validate-vampire-suspect {:name "Mr Poopy Butthole" :haha #"10/10"}) ; => false
 
-; (def vamp-suspects (mapify (parse (slurp filename))))
+;; (def vamp-suspects (mapify (parse (slurp filename))))
 ;; (= vamp-suspects
 ;;    (safe-append vamp-suspects {:name "Mr Poopy Butthole" :haha #"10/10"})) ; => true
 
-; (unparse-suspects vamp-suspects)
-; => "Edward Cullen,10\nBella Swan,0\nCharlie Swan,0\nJacob Black,3\nCarlisle Cullen,6"
-
+;; (unparse-suspects vamp-suspects)
+;; => "Edward Cullen,10\nBella Swan,0\nCharlie Swan,0\nJacob Black,3\nCarlisle Cullen,6"
 
